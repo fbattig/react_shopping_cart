8465b6d8f71970886a9b897408730387e3cf61ea
Felix Battig <fbattig@yahoo.com>
2018-12-14 10:21:29 -0500

    completed

--- src/features/pages/HomePage/HomePage.js
+++ src/features/pages/HomePage/HomePage.js
@@ -5,7 +5,7 @@ import data from '../../../data/products.json'
 export class HomePage extends Component {
   render() {
     return (
-      <div>
+      <div >
         <h1>Home Page</h1>
         <Products products={data.products}/>
       </div>
